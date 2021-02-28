fcf14f4ba93c9bd7a957beada0b7193e322c9182
carjeff <747938871@qq.com>
2021-02-28 13:59:20 +0800

    初始化项目“

---
+++ employment-server/src/main/java/com/buct/employment/EmploymentApplication.java
@@ -0,0 +1,13 @@
+package com.buct.employment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentApplication.class, args);
+    }
+
+}
---
+++ employment-server/src/test/java/com/buct/employment/EmploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buct.employment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
